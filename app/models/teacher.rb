adfca9dec9c1f1ffed1514d67069bbecf44f64a5
GenTokunaga <GenTokunaga@users.noreply.github.com>
2026-02-19 21:30:15 +0900

    fix: ブロック引数をaからattachableに(ついでに画像表示部分リファクタ)

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -2,8 +2,8 @@ class Teacher < ApplicationRecord
   devise :database_authenticatable, :recoverable, :rememberable, :validatable, :trackable
 
   has_many :lesson_slots, dependent: :destroy
-  has_one_attached :profile_image do |a|
-    a.variant :thumb, resize_to_limit: [200, 200], preprocessed: true
+  has_one_attached :profile_image do |attachable|
+    attachable.variant :thumb, resize_to_limit: [200, 200], preprocessed: true
   end
 
   extend Enumerize
