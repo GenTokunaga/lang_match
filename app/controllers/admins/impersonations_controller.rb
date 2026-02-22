364ffe2bb2feb373dc97de88f11381f95b71db84
GenTokunaga <GenTokunaga@users.noreply.github.com>
2026-02-22 21:49:38 +0900

    fix: 講師なりすましログアウト時にadminの再ログインをしないように修正(routeもid指定なしに)

--- app/controllers/admins/impersonations_controller.rb
+++ app/controllers/admins/impersonations_controller.rb
@@ -1,6 +1,5 @@
 class Admins::ImpersonationsController < Admins::BaseController
   before_action :set_admins_teacher, only: %i[create]
-  before_action :set_admin, only: %i[destroy]
 
   def create
     session[:impersonator_admin_id] = current_admin.id
@@ -11,7 +10,6 @@ class Admins::ImpersonationsController < Admins::BaseController
   def destroy
     session.delete(:impersonator_admin_id)
     sign_out(:teacher)
-    sign_in(:admin, @admin)
     redirect_to admins_root_path, notice: '講師からログアウトしました'
   end
 
@@ -19,9 +17,5 @@ class Admins::ImpersonationsController < Admins::BaseController
 
   def set_admins_teacher
     @admins_teacher = Teacher.find(params.expect(:id))
-  end
-
-  def set_admin
-    @admin = Admin.find(params.expect(:id))
   end
 end
